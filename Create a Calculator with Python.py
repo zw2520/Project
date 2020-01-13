0ea99df0fcfe07c7c6f1df91844488fe72f2c3de
zw2520 <44385297+zw2520@users.noreply.github.com>
2020-01-13 15:05:55 -0500

    Update and rename main.py to Create a Calculator with Python.py
    Adds more comments about the code

--- Create a Calculator with Python.py
+++ Create a Calculator with Python.py
@@ -10,13 +10,19 @@ def performMath():
     global run
     global previous
     equation = ""
+    
+    #make the calculation to be continuous
     if previous == 0:
         equation = input("Enter Equation: ")
     else:
         equation = input(str(previous))
+    
+    # Exit Option
     if equation == 'quit':
         print("Goodbye! :)")
         run = False
+    
+    #To solve the problem with Eval function
     else:
         equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
         if previous == 0:
