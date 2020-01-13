1ab0ab58511fcd55aaed549689716821db898d5d
wangzheng <zw2520@columbia.edu>
2020-01-13 15:07:29 -0500

    Reformat it

--- Create a Calculator with Python.py
+++ Create a Calculator with Python.py
@@ -6,23 +6,24 @@ print("Type 'quit' for Exit\n")
 previous = 0
 run = True
 
+
 def performMath():
     global run
     global previous
     equation = ""
-    
-    #make the calculation to be continuous
+
+    # make the calculation to be continuous
     if previous == 0:
         equation = input("Enter Equation: ")
     else:
         equation = input(str(previous))
-    
+
     # Exit Option
     if equation == 'quit':
         print("Goodbye! :)")
         run = False
-    
-    #To solve the problem with Eval function
+
+    # To solve the problem with Eval function
     else:
         equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
         if previous == 0:
@@ -30,6 +31,6 @@ def performMath():
         else:
             previous = eval(str(previous) + equation)
 
+
 while run:
     performMath()
-
